f2300059e6af91cdf862f181f80f84f6c2370c7b
appliedrd <ted@appliedrd.com>
2022-10-28 19:10:34 -0400

    dispatch key press and listen

--- src/components/Rt.js
+++ src/components/Rt.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
 import flankerImg from '../incongruentleft.png';
+import {CustomTextInput} from './customTextInput';
 
 export default class Rt extends Component {
     constructor(props) {
         super(props);
+        this.rtInput = null;
+
+        this.setRtInputRef = element => {
+            this.rtInput = element;
+        };
+
+        this.rtInputSetter = () => {
+            // Focus the text input using the raw DOM API
+            if (this.rtInput) {
+                console.log("rtInputSetter")
+                this.rtInput.value = "hello";
+            } else {
+                console.log("no rtInputSetter")
+            }
+        };
 
         // Initializing the state 
         this.state = {
@@ -22,24 +38,27 @@ export default class Rt extends Component {
             },
             data: [],
             allowedKeys: [37, 39],
-            isStarted:true
+            isStarted: true
         };
         this.buttonText = "Stop"
         this.keyPressed = null
         this.rt = null
         this.gotKey = false
-        this.results = <p>Initializing</p> 
+        this.results = <p>Initializing</p>
     }
     componentDidMount() {
-
         // Changing the state after 2 sec
         // from the time when the component
         // is rendered
         setTimeout(() => {
             this.setState({ color: 'wheat' })
         }, 2000);
-        this.animate()
+        // this.animate()
         this.intervalId = setInterval(this.timer.bind(this), 1000)
+        this.rtInputSetter()
+        this.rtInput.addEventListener('keypress', (event) => {
+            console.log("window keypress")
+          });
     }
 
     componentWillUnmount() {
@@ -53,7 +72,7 @@ export default class Rt extends Component {
         } else {
             clearInterval(this.intervalId)
         }
-      }
+    }
 
 
     animate = () => {
@@ -82,6 +101,7 @@ export default class Rt extends Component {
     }
 
     handler = (event) => {
+        console.log('key hit')
         if (this.timing) {
             this.rt = new Date() - this.rt
             this.gotKey = true
@@ -91,11 +111,18 @@ export default class Rt extends Component {
         }
     };
 
+    // This is simply an example that demonstrates
+    // how you can dispatch an event on the element.
+    triggerKeyPress() {
+        this.rtInput.dispatchEvent(new KeyboardEvent('keypress', { 'key': 'a' }
+        ))
+    }
+
 
     render() {
         let val
         let flankerI
-        let bText= this.state.isStarted? "STOP":"START";
+        let bText = this.state.isStarted ? "STOP" : "START";
 
         if (this.state.currentCount >= 4 && this.state.currentCount <= 7) {
             if (this.state.currentCount == 7) {
@@ -103,11 +130,17 @@ export default class Rt extends Component {
                 this.timing = true
                 this.rt = new Date()
                 this.key = false
+
             }
             val = <p>{this.state.currentCount}</p>
             flankerI = <p><img src={flankerImg} /></p>
-            let element = document.querySelector('input');
+            // this.divRef.dispatchEvent(new KeyboardEvent('keypress', { 'key': 'a' }));
+            // this.triggerKeyPress()
+        } else if (this.state.currentCount == 2) {
+            console.log("2")
+            this.triggerKeyPress()
         } else if (this.state.currentCount > 7) {
+            //this.clearInput()
             val = <p>preparing...</p>
             this.timing = false
             this.results = <p></p>
@@ -120,30 +153,40 @@ export default class Rt extends Component {
         }
         return (
             <React.Fragment>
-            <div>
-                <div
-                    style={{
-                        textAlign: 'center',
-                        paddingTop: 20,
-                        width: 600,
-                        height: 80,
-                        margin: 'auto'
-                    }}>
-                    <h1> Realtime keydown event test</h1>
-                    <p> hit a key as soon as you see the image</p>
-                    <p> debug info in chrome inspect console</p>
-                    <button onClick={() => {
-                        this.setState({isStarted: !this.state.isStarted})
-                        this.startStop()
+                <div>
+                    <div
+                        style={{
+                            textAlign: 'center',
+                            paddingTop: 20,
+                            width: 600,
+                            height: 80,
+                            margin: 'auto'
+                        }}>
+                        <h1> Realtime keydown event test</h1>
+                        <p> hit a key as soon as you see the image</p>
+                        <p> debug info in chrome inspect console</p>
+                        <button onClick={() => {
+                            this.setState({ isStarted: !this.state.isStarted })
+                            this.startStop()
                         }}>{bText}</button>
-                    <div>
-                        {val}
-                        {flankerI}
+                        <div>
+                            {val}
+                            {flankerI}
+                        </div>
+                        <input
+                            ref={this.setRtInputRef}
+                            id="rtInput"
+                            contentEditable={true}
+                            placeholder="type a message"
+                            data-reactid="137"
+                            onKeyPress={(e) => this.handler(e)}
+                        />
+                        <CustomTextInput
+                            inputRef={el => this.inputElement = el}
+                        />
+                        <div>{this.results}</div>
                     </div>
-                    <input id="rtInput" type="text" onKeyPress={(e) => this.handler(e)} />
-                    <div>{this.results}</div>
                 </div>
-            </div>
             </React.Fragment>
         );
     }
